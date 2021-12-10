818ae0e92ca08bb0c3ef42a35ddec8cb3f60a418
harbouli <mharbouli@icloud.com>
2021-12-10 16:53:40 +0100

    Application With Stream Chat

---
+++ src/components/ChannelListContainer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { ChannelList, useChatContext } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+
+// const SideBar ()=>(
+//     <div className="channel-list__sidebar">
+//         c
+//     </div>
+// )
+
+const ChannelListContainer = () => {
+  return <div></div>;
+};
+s;
+export default ChannelListContainer;
