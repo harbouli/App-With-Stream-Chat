60e3652914664e559c3e53397c4ec3cacf149b36
harbouli <mharbouli@icloud.com>
2021-12-10 16:54:21 +0100

    Application With Stream Chat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
 
-function App() {
+import { ChannelContainer, ChannelListContainer } from "./components";
+
+import "./App.css";
+
+const apiKey = "s5snmfrcx8ew";
+
+const client = StreamChat.getInstance(apiKey);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app__wrapper">
+        <Chat client={client} theme="team light">
+          <ChannelListContainer />
+          <ChannelContainer />
+        </Chat>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
